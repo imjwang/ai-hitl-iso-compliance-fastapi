b4a2be9b3befd0215378679940a289be34005abe
imjwang <ngjaywa@gmail.com>
2024-11-02 20:25:00 -0700

    build

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
 
 if __name__ == "__main__":
     import uvicorn
